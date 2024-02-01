b0aa126a05ea5f334049096c0f940b326a330e81
swapnilranmale2003 <swapnilranmale2003@gmail.com>
2024-02-01 12:58:36 +0530

    Binding Event Handlers

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,32 @@
-import {useEffect,useState} from 'react'
+// import {useEffect,useState} from 'react'
 // import { getPosts } from './api/getapi'
-import { getComments } from './api/getCommentsApi';
+// import { getComments } from './api/getCommentsApi';
 
 import './App.css';
-
+import ClassClick from './components/ClassClick';
+import EventBinding from './components/EventBinding';
+import FunctionClick from './components/FunctionClick';
 function App() {
-  const [data,setData]=useState(null);
-  const [edata,setEdata]=useState(null);
-  useEffect (()=>{
-    // getPosts().then(posts=>setData(posts));
-    getComments().then(comments => setEdata(comments));
+  // const [data,setData]=useState(null);
+  // const [edata,setEdata]=useState(null);
+  // useEffect (()=>{
+  //   getPosts().then(posts=>setData(posts));
+  //   getComments().then(comments => setEdata(comments));
 
-  },[])
+  // },[])
   return (
     <div className="App">
-     {
+     {/* {
 
       // data ? data.map((e)=> <li>{e.title}</li> )  :"no data"
       edata ? edata.map((e)=> <li>{e.email}{e.name}</li> ) : "no data"
+     } */
+    
+    //  <FunctionClick />
+    //  <ClassClick />
+     <EventBinding/>
+     
+    
      }
     </div>
   );
