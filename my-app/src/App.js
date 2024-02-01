c69348aeb63d3d998bc9974d793ab2ceb47c8c30
swapnilranmale2003 <swapnilranmale2003@gmail.com>
2024-02-01 11:52:53 +0530

    Rendered email,name from another api

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,20 +1,23 @@
 import {useEffect,useState} from 'react'
-import { getPosts } from './api/getapi'
-// import { getComments } from './api/getCommentsApi';
+// import { getPosts } from './api/getapi'
+import { getComments } from './api/getCommentsApi';
 
 import './App.css';
 
 function App() {
   const [data,setData]=useState(null);
+  const [edata,setEdata]=useState(null);
   useEffect (()=>{
-    getPosts().then(posts=>setData(posts));
-    // getComments().then(comments=>console.log(comments));
+    // getPosts().then(posts=>setData(posts));
+    getComments().then(comments => setEdata(comments));
 
   },[])
   return (
     <div className="App">
      {
-      data ? data.map((e)=> <li>{e.title}</li> )  :"no data"
+
+      // data ? data.map((e)=> <li>{e.title}</li> )  :"no data"
+      edata ? edata.map((e)=> <li>{e.email}{e.name}</li> ) : "no data"
      }
     </div>
   );
