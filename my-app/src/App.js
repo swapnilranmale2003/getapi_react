8e005d0ec75f56cce718c18089bea4d31585b9a8
swapnilranmale2003 <swapnilranmale2003@gmail.com>
2024-02-01 11:43:48 +0530

    Render the data on webpage

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,18 +1,21 @@
-import {useEffect} from 'react'
+import {useEffect,useState} from 'react'
 import { getPosts } from './api/getapi'
-import { getComments } from './api/getCommentsApi';
+// import { getComments } from './api/getCommentsApi';
 
 import './App.css';
 
 function App() {
+  const [data,setData]=useState(null);
   useEffect (()=>{
-    getPosts().then(posts=>console.log(posts));
-    getComments().then(comments=>console.log(comments));
+    getPosts().then(posts=>setData(posts));
+    // getComments().then(comments=>console.log(comments));
 
   },[])
   return (
     <div className="App">
-     
+     {
+      data ? data.map((e)=> <li>{e.title}</li> )  :"no data"
+     }
     </div>
   );
 }
