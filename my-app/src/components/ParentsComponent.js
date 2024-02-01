53b9ce9aeb4bda8901565c20889adcbc3512dc76
swapnilranmale2003 <swapnilranmale2003@gmail.com>
2024-02-01 14:15:46 +0530

    pass the para from child to parent

--- my-app/src/components/ParentsComponent.js
+++ my-app/src/components/ParentsComponent.js
@@ -10,8 +10,8 @@ class ParentsComponent extends Component {
       }
       this.greetParent=this.greetParent.bind(this)
     }
-    greetParent(){
-        alert(`Hello ${this.state.parentName}`)
+    greetParent(childName){
+        alert(`Hello ${this.state.parentName} from ${childName}`)
     }
     
   render() {
